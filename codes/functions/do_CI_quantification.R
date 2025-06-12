a14816901f1961d349997b613f27c6c9c4c105a8
yeonjucho <45888834+yeonjucho@users.noreply.github.com>
2025-06-12 16:16:45 -0400

    update

--- codes/functions/do_CI_quantification.R
+++ codes/functions/do_CI_quantification.R
@@ -2,39 +2,45 @@
 #  do_Cellular_Influence_(CI)_quantification  #
 ###############################################
 
-
-do_CI_quantification<- function(expr = df_rm, # should contain sample_id, X, Y position, celltype in the df
-                                sampleID = sampleID,
-                                kernels ="gaussian",
-                                clusterlevels = clusterlevels,
-                                sigma = 10){
+do_CI_quantification <- function(expr = df_rm,
+                                 sampleID = sampleID,
+                                 x_col = "X_position",
+                                 y_col = "Y_position",
+                                 celltype_col = "celltype",
+                                 sample_col = "sample_id",
+                                 kernels = "gaussian",
+                                 clusterlevels = clusterlevels,
+                                 sigma = 10) {
   require(spatstat);
   
   spatwt<- c()
-  for (i in sampleID){  
-    expr_k<- expr[expr$sample_id==i, ]
-    coord <- expr_k[, c("X_position", "Y_position")]
+  for (i in sampleID){
+    expr_k <- expr[expr[[sample_col]] == i, ]
+    coord <- expr_k[, c(x_col, y_col)]
     
-    rownames(coord)<- paste0(rownames(expr_k), ":", expr_k$celltype)
+    rownames(coord) <- paste0(rownames(expr_k), ":", expr_k[[celltype_col]])
     
-    gaussian<- data.frame(matrix(0, nrow = nrow(coord), ncol = length(clusterlevels)))
-    colnames(gaussian) <- clusterlevels
+    df<- data.frame(matrix(0, nrow = nrow(coord), ncol = length(clusterlevels)))
+    colnames(df) <- clusterlevels
     
-    coord <- cbind(coord, gaussian)
+    coord <- cbind(coord, df)
     
     sapply(1:nrow(coord), function(r)
       coord[r, as.character(lapply(strsplit(as.character(rownames(coord)[r]), split = ":"),"[[",2))] <<- 1)
     
     # calculation of spatial weight 
-    mywin <- owin(xrange = range(coord$X_position), yrange = range(coord$Y_position))
-    spObj <- ppp(x=coord$X_position,y=coord$Y_position,window = mywin, marks = coord[,3:length(coord)])
+    mywin <- owin(xrange = range(coord[[x_col]]), yrange = range(coord[[y_col]]))
+    spObj <- ppp(x = coord[[x_col]],
+                 y = coord[[y_col]],
+                 window = mywin,
+                 marks = coord[, clusterlevels])
     
     # adjust sigma 
     # leaveoneout = T -> removing the self
-    K <- Smooth(spObj,kernel = kernels,at = "points",leaveoneout = T, sigma=sigma) 
-    rownames(K)<-rownames(coord)
-    K<- as.data.frame(K)
-    K$sample_id <- i 
+    K <- Smooth(spObj, kernel = kernels, at = "points", leaveoneout = TRUE, sigma = sigma)
+    rownames(K) <- rownames(coord)
+    K <- as.data.frame(K)
+    K[[sample_col]] <- i
     
     spatwt<- rbind(spatwt, K)
   } 
