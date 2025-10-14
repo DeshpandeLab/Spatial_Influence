90fbea70ce8da69a105df1894f5796f6663c2cc6
yeonjucho <45888834+yeonjucho@users.noreply.github.com>
2025-10-14 09:54:39 -0400

    update for single sample computation

--- codes/functions/do_CI_quantification.R
+++ codes/functions/do_CI_quantification.R
@@ -3,47 +3,72 @@
 ###############################################
 
 do_CI_quantification <- function(expr = df_rm,
-                                 sampleID = sampleID,
                                  x_col = "X_position",
                                  y_col = "Y_position",
                                  celltype_col = "celltype",
-                                 sample_col = "sample_id",
+                                 multisample = FALSE, 
+                                 sample_col = NA, 
                                  kernels = "gaussian",
-                                 clusterlevels = clusterlevels,
                                  sigma = 10) {
   require(spatstat);
   
+  # arrange the data column with cell type levels
+  clusterlevels <- if (is.factor(expr[[celltype_col]]) && length(levels(expr[[celltype_col]])) > 0){
+    levels(expr[[celltype_col]])} else {unique(expr[[celltype_col]])}
+  
+  
   spatwt<- c()
-  for (i in sampleID){
-    expr_k <- expr[expr[[sample_col]] == i, ]
-    coord <- expr_k[, c(x_col, y_col)]
+  # single sample 
+  if (!multisample) {
+    expr_k <- expr
+    coord  <- expr_k[, c(x_col, y_col)]
     
     rownames(coord) <- paste0(rownames(expr_k), ":", expr_k[[celltype_col]])
     
-    df<- data.frame(matrix(0, nrow = nrow(coord), ncol = length(clusterlevels)))
+    df <- data.frame(matrix(0, nrow = nrow(coord), ncol = length(clusterlevels)))
     colnames(df) <- clusterlevels
-    
     coord <- cbind(coord, df)
     
-    sapply(1:nrow(coord), function(r)
-      coord[r, as.character(lapply(strsplit(as.character(rownames(coord)[r]), split = ":"),"[[",2))] <<- 1)
+    sapply(seq_len(nrow(coord)), function(r)
+      coord[r, as.character(lapply(strsplit(rownames(coord)[r], ":"), "[[", 2))] <<- 1)
     
-    # calculation of spatial weight 
     mywin <- owin(xrange = range(coord[[x_col]]), yrange = range(coord[[y_col]]))
-    spObj <- ppp(x = coord[[x_col]],
-                 y = coord[[y_col]],
-                 window = mywin,
-                 marks = coord[, clusterlevels])
+    spObj <- ppp(x = coord[[x_col]], y = coord[[y_col]], window = mywin, marks = coord[, clusterlevels])
     
-    # adjust sigma 
-    # leaveoneout = T -> removing the self
+    # calculation of spatial weight
     K <- Smooth(spObj, kernel = kernels, at = "points", leaveoneout = TRUE, sigma = sigma)
     rownames(K) <- rownames(coord)
-    K <- as.data.frame(K)
-    K[[sample_col]] <- i
+    spatwt <- K
     
-    spatwt<- rbind(spatwt, K)
-  } 
+  } else {
+    # multiple samples case 
+    spatwt <- c()
+    sampleID <- unique(expr[[sample_col]])
+    for (i in sampleID) {
+      expr_k <- expr[expr[[sample_col]] == i, ]
+      coord  <- expr_k[, c(x_col, y_col)]
+      
+      rownames(coord) <- paste0(rownames(expr_k), ":", expr_k[[celltype_col]])
+      
+      df <- data.frame(matrix(0, nrow = nrow(coord), ncol = length(clusterlevels)))
+      colnames(df) <- clusterlevels
+      coord <- cbind(coord, df)
+      
+      sapply(seq_len(nrow(coord)), function(r)
+        coord[r, as.character(lapply(strsplit(rownames(coord)[r], ":"), "[[", 2))] <<- 1)
+      
+      mywin <- owin(xrange = range(coord[[x_col]]), yrange = range(coord[[y_col]]))
+      spObj <- ppp(x = coord[[x_col]], y = coord[[y_col]], window = mywin, marks = coord[, clusterlevels])
+      
+      K <- Smooth(spObj, kernel = kernels, at = "points", leaveoneout = TRUE, sigma = sigma)
+      rownames(K) <- rownames(coord)
+      K <- as.data.frame(K)
+      K[[sample_col]] <- i
+      
+      spatwt <- rbind(spatwt, K)
+    }
+  }
+  
   
   return (spatwt) 
 }
